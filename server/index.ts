c3ef32c789b59b23d4628c102cc97dcc36de1681
User <user@example.com>
2025-11-16 12:48:17 +0530

    Fix CORS: allow frontend access from Surge.sh

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,19 @@ import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
 
+// Enable CORS for frontend
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 declare module 'http' {
   interface IncomingMessage {
     rawBody: unknown
